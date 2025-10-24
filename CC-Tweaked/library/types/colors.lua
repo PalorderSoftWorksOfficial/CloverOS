10654758abcb2e610978651f45a12531687504de
PalorderSoftWorksOfficial <palordersoftworks@gmail.com>
2025-10-24 13:32:56 +0200

    Delete CC-Tweaked directory

--- CC-Tweaked/library/types/colors.lua
+++
@@ -1,5 +0,0 @@
----@meta
-
----@alias ccTweaked.colors.color integer An integer represeting a color value. Colors can be found in the `colors` namespace, such as `colors.orange`
-
----@alias ccTweaked.colors.colorSet integer A set of colors (an integer representing multiple color values)
--- CC-Tweaked/library/types/general.lua
+++
@@ -1,3 +0,0 @@
----@meta
-
----@alias ccTweaked.epoch number A number of milliseconds since the [UNIX epoch](https://en.wikipedia.org/wiki/Unix_time). Useful for timestamping
