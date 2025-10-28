9ec2b2cb93f72ce7bc8544607f616bc0d8ef29b6
PalorderSoftWorksOfficial <PalorderSoftWorksOfficial@gmail.com>
2025-10-28 11:21:27 +0100

    Sigh forgot to recode sum shit

--- startup.lua
+++ startup.lua
@@ -1,4 +1,4 @@
-local filePath = (function() for i=0,99 do local d="disk"..(i==0 and "" or i) if fs.exists(d.."/instructions.txt") then return d.."/instructions.txt" end end return "/etc/instructions.txt" end)()
+local filePath=(function()for i=0,99 do local d="disk"..(i==0 and""or i)if fs.exists(d.."/instructions.txt")then return d.."/instructions.txt"end end if fs.exists("/instructions.txt")then return"/instructions.txt"end return"/etc/instructions.txt"end)()
 
 if fs.exists(filePath) then
     local file = fs.open(filePath, "r")
@@ -10,7 +10,7 @@ if fs.exists(filePath) then
     
     print("\nAuto booting CloverOS in 10 seconds...")
     os.sleep(10)
-    shell.run((function() for i=0,99 do local d="disk"..(i==0 and "" or i) if fs.exists(d.."/CloverOS_OS.lua") then return d.."/CloverOS_OS.lua" end end end)())
+    shell.run((function() for i=0,99 do local d="disk"..(i==0 and "" or i) if fs.exists(d.."/CloverOS_OS.lua") then return d.."/CloverOS_OS.lua" elseif fs.exists("/CloverOS_OS") then return "/CloverOS_OS.lua" end end end)())
 else
     print("Instructions file not found. Please ensure the disk is inserted correctly.")
 end
