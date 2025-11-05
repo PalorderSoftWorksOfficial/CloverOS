8641cc5bd6ee8864abaf3045abb4b2f97aff6ea1
KomVorm! <KomVorm!@DESKTOP-K9QDRG1>
2025-11-05 03:12:43 -0800

    Updated startp and ccemux, and init custom ccemux API

--- startup.lua
+++ startup.lua
@@ -119,3 +119,13 @@ osAPIFunc = {
 }
 osAPI = osAPIFunc
 GDI = osAPI.GDI
+if ccemux then
+    for k,v in pairs(ccemux_open_with_size_temp)do
+        _G.ccemux[k]=v
+    end
+    ccemux.openEmuWithComputerSize=function(id)return ccemux.openEmuWithSize(id,51,19) end
+    ccemux.openEmuWithPocketComputerSize=function(id)return ccemux.openEmuWithSize(id,26,20) end
+    ccemux.openEmuWithDefaultMonitorSize=function(id)return ccemux.openEmuWithSize(id,8,6) end
+    ccemux.openEmuWithTurtleSize=function(id)return ccemux.openEmuWithSize(id,39,13) end
+    _G.ccemux_open_with_size_temp=nil
+end
