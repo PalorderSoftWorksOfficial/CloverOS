d95648b256b5b2d7d90c8ddcadce7461a228d507
Digibende <Digibende@DB-DB-MT-01X>
2025-11-10 12:29:53 +0100

    Updated boot binary

--- startup.lua
+++ startup.lua
@@ -92,6 +92,17 @@ local DISK_ROOT = (function()
     if fs.exists("/") and fs.exists("/CloverOS_OS.lua") then return "/" end
     return nil
 end)()
+function mergeTables(t1, t2)
+    for k, v in pairs(t2) do
+        if type(v) == "table" and type(t1[k]) == "table" then
+            mergeTables(t1[k], v)
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+local API = require(DISK_ROOT.."/CloverOS_API.lua")
 osAPIFunc = {
    version = function ()
     return "CloverOS v1.0.0"
@@ -104,8 +115,11 @@ osAPIFunc = {
     end,
     GDI = GDI,
 }
+
+mergeTables(API, osAPIFunc)
 osAPI = osAPIFunc
 GDI = osAPI.GDI
+
 os.sleep(1)
 if not DISK_ROOT then
     print("Error: could not detect running disk or root installation")
