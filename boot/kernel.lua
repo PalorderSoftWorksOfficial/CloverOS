625bc85bd35592dd737c34de330731c396b97257
Digibende <Digibende@DB-DB-MT-01X>
2026-03-03 14:01:00 +0100

    rewritten kernel

--- boot/kernel.lua
+++ boot/kernel.lua
@@ -1,1 +1,20 @@
-local function findOS()for i=0,99 do local d="/disk"..(i==0 and""or i).."/CloverOS_OS.lua" if fs.exists(d) then return d end end if fs.exists("/boot/CloverOS_OS.lua") then return "/boot/CloverOS_OS.lua" end error("CloverOS not found")end;local path=findOS();local oldG={};for k,v in pairs(_G)do oldG[k]=v end;oldG._G=oldG;_G={};for k,v in pairs(oldG)do _G[k]=v end;_G._G=_G;local nativeTerm=require("term").native();local newTerm={};for k,v in pairs(require("term"))do newTerm[k]=v end;for k,v in pairs(nativeTerm)do newTerm[k]=v end;_G.term=newTerm;local fn,err=loadfile(path,"bt",_G);if not fn then printError(err)return end;local ok,err=pcall(fn);if not ok then printError(err) end;while true do os.pullEvent() end
+local function findOS()for i=0,99 do local d="/disk"..(i==0 and""or i).."/CloverOS_OS.lua" if fs.exists(d) then return d end end if fs.exists("/boot/CloverOS_OS.lua") then return "/boot/CloverOS_OS.lua" end error("CloverOS not found") end
+local path=findOS()
+local oldG=_G
+local _G={}
+_G._G=_G
+
+local rom=oldG._ROM or {}
+local function restoreAPI(name)
+    if not _G[name] then
+        _G[name]=oldG[name] or (rom[name] or require(name))
+    end
+end
+for _,v in ipairs{"term","fs","os","colors","shell","io","bit","textutils","pocket"} do restoreAPI(v) end
+if not _G.term.native then _G.term.native=_G.term end
+
+local fn,err=loadfile(path,"bt",_G)
+if not fn then printError(err)return end
+local ok,err=pcall(fn)
+if not ok then printError(err) end
+while true do _G.os.pullEvent() end
