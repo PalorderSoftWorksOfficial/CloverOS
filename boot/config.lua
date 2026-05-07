4843e2302db468c08164fe8c3c791418f746611f
Digibende <Digibende@Digibende.nl>
2026-05-07 15:28:44 +0200

    hmmm cheese

--- boot/config.lua
+++ boot/config.lua
@@ -1,28 +1,30 @@
 defaultentry = "CloverOS"
 timeout = 5
-
+backgroundcolor = colors.black
+selectcolor = colors.orange
+titlecolor = colors.lightGray
 local function findCloverRoot()
-  if fs.exists("/CloverOS_OS.lua") and fs.exists("/CloverOS_API.lua") then
-    return "/"
-  end
-  for i = 0, 99 do
-    local root = "/disk" .. (i == 0 and "" or i)
-    if fs.exists(root .. "/CloverOS_OS.lua") and fs.exists(root .. "/CloverOS_API.lua") then
-      return root
-    end
-  end
-  return "/"
+	if fs.exists("/CloverOS_OS.lua") and fs.exists("/CloverOS_API.lua") then
+		return "/"
+	end
+	for i = 0, 99 do
+		local root = "/disk" .. (i == 0 and "" or i)
+		if fs.exists(root .. "/CloverOS_OS.lua") and fs.exists(root .. "/CloverOS_API.lua") then
+			return root
+		end
+	end
+	return "/"
 end
 
 local ROOT = findCloverRoot()
 local KERNEL = ROOT == "/" and "/kernel.lua" or ROOT .. "/kernel.lua"
 
-menuentry "CloverOS" {
-  description "Boot CloverOS.",
-  chainloader(KERNEL),
-}
+menuentry("CloverOS")({
+	description("Boot CloverOS."),
+	chainloader(KERNEL),
+})
 
-menuentry "CraftOS" {
-  description "Boot into CraftOS.",
-  craftos,
-}
+menuentry("CraftOS")({
+	description("Boot into CraftOS."),
+	craftos,
+})
